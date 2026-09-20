bb9a4f4fb6bbed0dd13972c708b70efc3837b9e9
sanoken0201-beep <sanoken0201@gmail.com>
2026-09-20 20:50:07 +0900

    Require rake regression in production release gate

--- test_suites.py
+++ test_suites.py
@@ -32,6 +32,7 @@ TEST_SUITES: dict[str, tuple[str, ...]] = {
         "smoke_test_poker_simple.py",
         "smoke_test_oop_check_browser.py",
         "smoke_test_poker_control_audit.py",
+        "smoke_test_rake_settlement_fix.py",
     ),
     "browser_contract": (
         "smoke_test_prebuilt_assets.py",
@@ -111,6 +112,7 @@ PRODUCTION_RELEASE_SELECTION: tuple[tuple[str, tuple[str, ...]], ...] = (
     ("ring_gameplay", (
         "smoke_test_v1244.py",
         "smoke_test_single_public_table.py",
+        "smoke_test_rake_settlement_fix.py",
     )),
     ("sitngo", (
         "smoke_test_sitngo_phase1.py",
