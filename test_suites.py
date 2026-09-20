c7b29a5032f5e2d63c08a65b8a938c544f8fabc9
sanoken0201-beep <sanoken0201@gmail.com>
2026-09-21 04:26:39 +0900

    Own Sit&Go process and resilience release tests

--- test_suites.py
+++ test_suites.py
@@ -57,6 +57,8 @@ TEST_SUITES: dict[str, tuple[str, ...]] = {
     "sitngo": (
         "smoke_test_sitngo_phase1.py",
         "smoke_test_sitngo_gameplay.py",
+        "smoke_test_sitngo_process_model.py",
+        "smoke_test_sitngo_resilience.py",
         "smoke_test_sitngo_points.py",
         "smoke_test_sitngo_chip_rules.py",
         "smoke_test_sitngo_tournament_rules.py",
@@ -66,6 +68,8 @@ TEST_SUITES: dict[str, tuple[str, ...]] = {
         "smoke_test_sitngo_freezeout_contract.py",
         "smoke_test_sitngo_api.py",
         "smoke_test_sitngo_multi_session.py",
+        "smoke_test_sitngo_process_model.py",
+        "smoke_test_sitngo_resilience.py",
         "smoke_test_sitngo_browser.py",
     ),
 }
