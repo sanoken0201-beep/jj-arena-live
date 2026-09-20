759827ccc39d3c307b50aa3a687585806a90bbf7
sanoken0201-beep <sanoken0201@gmail.com>
2026-09-21 04:38:36 +0900

    Include Sit&Go build ownership in release contract

--- smoke_test_production_release_gate.py
+++ smoke_test_production_release_gate.py
@@ -50,6 +50,7 @@ def main() -> None:
             "smoke_test_rake_settlement_fix.py",
             "smoke_test_sitngo_phase1.py",
             "smoke_test_sitngo_gameplay.py",
+            "smoke_test_sitngo_build_ownership.py",
             "smoke_test_sitngo_process_model.py",
             "smoke_test_sitngo_resilience.py",
             "audit_ui_labels.py",
