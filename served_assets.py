beb682e5698dd8b597228f147e9109818f2b338f
sanoken0201-beep <sanoken0201@gmail.com>
2026-09-21 19:52:58 +0900

    Fix Sit&Go action turn token outage

--- served_assets.py
+++ served_assets.py
@@ -70,7 +70,7 @@ from read_efficiency import transform_app_js as transform_read_efficiency_app_js
 ROOT = Path(__file__).resolve().parent
 MATERIALIZED_STATIC = ROOT / "materialized_v1244" / "static"
 BUILD_ROOT = ROOT / ".jj_build"
-ASSET_VERSION = 71
+ASSET_VERSION = 72
 BUILD_FORMAT = 1
 
 _TODAYS_JJ_MARKER = "v2 today's-jj contrast hardening 2026-09-12"
